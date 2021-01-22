100a722914e50e6d148aebccf52cda3a460c5eec
zain-GFT <64900858+zain-GFT@users.noreply.github.com>
2021-01-22 12:19:30 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "firewall_rules" {
-  source = "modules/tf-gcp-firewall-rules"
+  source = "./modules/tf-gcp-firewall-rules"
   bastion_sa_email = module.service_account.bastion_sa_email
   shared_networking_id = var.shared_networking_id
   depends_on = [module.service_account]
 }
 
 module "service_account" {
-  source = "modules/tf-gcp-service-account"
+  source = "./modules/tf-gcp-service-account"
   main_iam_service_account_roles = var.main_iam_service_account_roles
   root_id = var.root_id
   sa_name = var.sa_name
@@ -14,7 +14,7 @@ module "service_account" {
 }
 
 module "bastion_linux" {
-  source = "modules/tf-gcp-bastion-linux-template"
+  source = "./modules/tf-gcp-bastion-linux-template"
   bastion_sa_email = module.service_account.bastion_sa_email
   bastion_subnetwork_name = var.bastion_subnetwork_name
   group_name = var.linux_group_name
@@ -29,7 +29,7 @@ module "bastion_linux" {
 }
 
 module "bastion_windows" {
-  source = "modules/tf-gcp-bastion-windows-template"
+  source = "./modules/tf-gcp-bastion-windows-template"
   bastion_sa_email = module.service_account.bastion_sa_email
   bastion_subnetwork_name = var.bastion_subnetwork_name
   group_name = var.windows_group_name
@@ -44,7 +44,7 @@ module "bastion_windows" {
 }
 
 module "attach_bastion" {
-  source = "modules/tf-gcp-attach-bastion"
+  source = "./modules/tf-gcp-attach-bastion"
   bastion_sa_email = module.service_account.bastion_sa_email
   bastion_subnetwork_name = var.bastion_subnetwork_name
   shared_bastion_id = var.shared_bastion_id
@@ -54,7 +54,7 @@ module "attach_bastion" {
 }
 
 module "windows_bastion_dns" {
-  source = "modules/tf-gcp-windows-bastion-dns"
+  source = "./modules/tf-gcp-windows-bastion-dns"
   private_dns_domain_name = var.private_dns_domain_name
   private_dns_name = var.private_dns_name
   shared_networking = var.shared_networking_id
@@ -63,7 +63,7 @@ module "windows_bastion_dns" {
 }
 
 module "linux_bastion_dns" {
-  source = "modules/tf-gcp-linux-bastion-dns"
+  source = "./modules/tf-gcp-linux-bastion-dns"
   linux_instances = module.bastion_linux.linux_bastion_instances
   private_dns_domain_name = var.private_dns_domain_name
   private_dns_name = var.private_dns_name
