bee95986fddc883193cd26033ea2219c1c9d2283
zain-GFT <64900858+zain-GFT@users.noreply.github.com>
2021-01-29 12:27:11 +0000

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,6 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+provider "google" {
+  region = var.region
+  zone   = var.region_zone
+}
+
+provider "google-beta" {
+  region = var.region
+  zone   = var.region_zone
+}
 
 terraform {
   required_providers {
